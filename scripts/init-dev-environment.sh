fd2a46040c5480b2ace212bdea272ef8e26aa784
Munif Tanjim <muniftanjim@gmail.com>
2021-05-28 20:40:59 +0600

    feat: add ssl for local dev

--- scripts/init-dev-environment.sh
+++ scripts/init-dev-environment.sh
@@ -1,34 +1,72 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+declare -r DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+declare -r ROOT_DIR="$(cd "${DIR}/.." && pwd)"
+
 command_exists() {
   type "${1}" >/dev/null 2>&1
+}
+
+ensure_tool() {
+  local -r tool="${1}"
+  if ! command_exists ${tool}; then
+    echo "Error: missing required tool: ${tool}!"
+    exit 1
+  fi
+}
+
+is_mac() {
+  [[ $OSTYPE = darwin* ]]
+}
+
+is_linux() {
+  [[ $OSTYPE = linux* ]]
 }
 
 setup_hostess() {
   if ! command_exists hostess; then
     echo "Installing: hostess"
 
-    if [[ $OSTYPE = darwin* ]]; then
-      if ! command_exists brew; then
-        echo "Error: missing required tool: brew!"
-        exit 1
-      fi
-
+    if is_mac; then
+      ensure_tool brew
       brew install hostess
     fi
 
-    if [[ $OSTYPE = linux* ]]; then
-      if ! command_exists wget; then
-        echo "Error: missing required tool: wget!"
-        exit 1
-      fi
+    if is_linux; then
+      ensure_tool wget
 
-      _hostess_version="0.5.2"
-      _hostess_url="https://github.com/cbednarski/hostess/releases/download/v${_hostess_version}/hostess_linux_amd64"
+      local -r hostess_version="0.5.2"
+      local -r hostess_url="https://github.com/cbednarski/hostess/releases/download/v${hostess_version}/hostess_linux_amd64"
 
-      wget -qN --show-progress ${_hostess_url} -O /tmp/hostess
+      wget -qN --show-progress ${hostess_url} -O /tmp/hostess
       chmod u+x /tmp/hostess
       sudo mv /tmp/hostess /usr/local/bin/hostess
+    fi
+
+    echo ""
+  fi
+}
+
+setup_mkcert() {
+  if ! command_exists mkcert; then
+    echo "Installing: mkcert"
+
+    if is_mac; then
+      ensure_tool brew
+      brew install mkcert
+    fi
+
+    if is_linux; then
+      ensure_tool wget
+
+      local -r mkcert_version="1.4.3"
+      local -r mkcert_url="https://github.com/FiloSottile/mkcert/releases/download/v${mkcert_version}/mkcert-v${mkcert_version}-linux-amd64"
+
+      wget -qN --show-progress ${mkcert_url} -O /tmp/mkcert
+      chmod u+x /tmp/mkcert
+      sudo mv /tmp/mkcert /usr/local/bin/mkcert
     fi
 
     echo ""
@@ -59,8 +97,30 @@ set_environment_variables() {
   echo ""
 }
 
+setup_dev_ssl() {
+  export CAROOT=$(mkcert -CAROOT)
+
+  local -r certs_dir="${ROOT_DIR}/docker/nginx/dev.certs"
+
+  mkdir -p "${certs_dir}"
+
+  echo "Generating Root certificate..."
+  mkcert -install
+  echo ""
+
+  echo "Generating SSL certificate..."
+  mkcert \
+    -cert-file "${certs_dir}/toolbox.local-cert.pem" \
+    -key-file "${certs_dir}/toolbox.local-key.pem" \
+    "toolbox.local" "*.toolbox.local"
+
+  ensure_tool certutil
+}
+
 setup_hostess
+setup_mkcert
 set_local_domains
 set_environment_variables
+setup_dev_ssl
 
 echo "Done!"
