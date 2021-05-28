c670d397fa5f14dea30129e92dfd542ed8868b33
Munif Tanjim <muniftanjim@gmail.com>
2021-05-28 13:10:45 +0600

    feat(backend): add utils/string/email

---
+++ apps/backend/src/utils/string/email.ts
@@ -0,0 +1,6 @@
+const pattern =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+export function isEmail(email: unknown): email is string {
+  return typeof email === 'string' && pattern.test(email);
+}
